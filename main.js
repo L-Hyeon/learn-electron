1357e735a30ed88bbee0f30bc17eb78ec664dc05
K <dkh1001@outlook.kr>
2022-05-05 00:24:43 +0900

    Simple Clock

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 const { app, BrowserWindow } = require("electron");
 
+app.whenReady().then(createWindow);
+
 function createWindow() {
-	// 브라우저 창을 생성
-	let win = new BrowserWindow({
-		width: 800,
-		height: 600,
-		webPreferences: {
-			nodeIntegration: true,
-		},
+	const win = new BrowserWindow({
+		width: 190,
+		height: 140,
+		resizable: false,
 	});
-
-	//브라우저창이 읽어 올 파일 위치
-	win.loadFile("./index.html");
+	win.loadFile("index.html");
 }
-
-app.on("ready", createWindow);
